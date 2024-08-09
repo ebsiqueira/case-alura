8cf1e1259fca539a7d8b12f157ce040c82ec4298
Eduardo Siqueira <eduardosiqueira26@gmail.com>
2024-08-09 19:38:44 -0300

    feat: create endpoint to frontend

---
+++ app/endpoints/pages_endpoints.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify, render_template, Blueprint
+from app.utils.feedback_utils import analyze_feedback, analyze_spam
+from app.utils.database_utils import *
+import requests
+import json
+
+pages = Blueprint('pages', __name__, template_folder='../templates')
+
+@pages.route('/home')
+def home():
+    return render_template('home.html')
+
+@pages.route('/send_feedback', methods=['GET', 'POST'])
+def send_feedback():
+    
+    if request.method == 'POST':
+        text = request.form.get('textarea')
+        
+        rq = requests.post("http://127.0.0.1:5000/feedback", json = {"feedback": text})
+        
+    
+    return render_template('send_feedback.html')
+
+@pages.route('/display_feedbacks')
+def display_feedbacks():
+    
+    feedbacks = select_feedbacks()
+    
+    return render_template('display_feedbacks.html')
