5707365071a9d755c35753563dcc73525a2bc516
Eduardo Siqueira <eduardosiqueira26@gmail.com>
2024-08-10 16:39:03 -0300

    refactor: update app name

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask
 from app.endpoints.feedback_endpoint import feedback
 from app.endpoints.pages_endpoints import pages
 
-app = Flask(__name__)
-app.register_blueprint(feedback)
-app.register_blueprint(pages)
+alumind = Flask(__name__)
+alumind.register_blueprint(feedback)
+alumind.register_blueprint(pages)
 
-app.run(debug=True)
+alumind.run(debug=True)
