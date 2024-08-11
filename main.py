2e0dd23937b7fc34b4e65f657b05f7ff87c86265
Eduardo Siqueira <eduardosiqueira26@gmail.com>
2024-08-11 14:03:42 -0300

    feat: add static folder in main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.endpoints.feedback_endpoint import feedback
 from app.endpoints.pages_endpoints import pages
 
 alumind = Flask(__name__)
+alumind._static_folder = 'app/static'
 alumind.register_blueprint(feedback)
 alumind.register_blueprint(pages)
 
